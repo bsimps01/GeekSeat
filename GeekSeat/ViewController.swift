55d13e05febbad14071589428434c084195f2a07
bsimps01 <b.albertsimpson@gmail.com>
2021-02-08 16:05:33 -0800

    added project

---
+++ GeekSeat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeekSeat
+//
+//  Created by Benjamin Simpson on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
